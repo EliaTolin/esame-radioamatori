93a38bbefb737560a54bed44dab44a955e249ce8
Gianluca Ciarcelluti <gianluca@ciarcelluti.it>
2025-11-15 19:30:30 +0100

    Segnali non sinusoidali

---
+++ scripts/plot_segnale_quadra.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Segnale quadra
+t = np.linspace(0, 4*np.pi, 1000)
+freq = 1 / (2*np.pi)  # frequenza per periodo 2pi
+square_wave = np.sign(np.sin(2*np.pi * freq * t))
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(t, square_wave, color='red', linewidth=2)
+ax.set_xlabel('Tempo (t)')
+ax.set_ylabel('Ampiezza')
+ax.set_title('Segnale Rettangolare (Quadra)')
+ax.grid(True)
+ax.axhline(0, color='black', linewidth=0.5)
+ax.set_yticks([-1, 0, 1])
+ax.set_yticklabels(['-1', '0', '1'])
+
+plt.tight_layout()
+plt.savefig('images/grafico_segnale_quadra.png', dpi=150)
+plt.close()
